78ad6cf2cf7e8527fbb1da7b43efb16c9dec590d
sneider trujillo <sneidertrujillo16@gmail.com>
2025-05-12 01:51:40 -0500

    Proyecto limpio sin node_modules ni dist

--- src/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-function ProtectedRoute({ children }) {
-  const isAuth = localStorage.getItem('auth');
-  return isAuth ? children : <Navigate to="/login" />;
-}
-
-export default ProtectedRoute;
